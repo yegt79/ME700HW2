0a99636c28048704dcfec408405b629796e34c1c
yegt79 <yegt@bu.edu>
2025-03-07 11:32:01 -0500

    Update and rename Direct_Stiffness_Methodd.py to Direct_Stiffness_Method.py

--- files/Direct_Stiffness_Method.py
+++ files/Direct_Stiffness_Method.py
@@ -1,8 +1,8 @@
 # Direct_Stiffness_Methodd.py
 import numpy as np
 import scipy.linalg as sp
-import functionss as fu
-from functionss import rotation_matrix_3D, transformation_matrix_3D
+import functions as fu
+from functions import rotation_matrix_3D, transformation_matrix_3D
 from typing import List, Dict, Tuple, Optional
 
 class BeamComponent:
